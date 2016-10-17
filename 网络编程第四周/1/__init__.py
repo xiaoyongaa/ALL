65b3e6f9b6a46f84a3886e6dce944e50215c222a
xiaoyonga <845219159@qq.com>
2016-10-17 20:12:21 +0800

    dasdd

--- 网络编程第四周/1/__init__.py
+++ 网络编程第四周/1/__init__.py
@@ -1,6 +1,29 @@
-h={}
-print(h,type(h))
-h[1]=[]
-h[1].append("das")
-print(h,type(h))
-print(h[1].pop)
+# h={}
+# print(h,type(h))
+# h[1]=[]
+# h[1].append("das")
+# print(h,type(h))
+# print(h[1].pop)
+
+
+
+class f1:
+    def f1(self):
+        print("f1")
+
+
+class f2:
+    def f1(self):
+        print("f2")
+        print(self.f1())
+
+
+
+class f3(f2,f1):
+    def f1(self):
+        print("f3")
+
+
+
+obj=f3()
+obj.f1()
