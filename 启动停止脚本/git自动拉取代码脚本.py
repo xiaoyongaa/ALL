c19ab64df70e565cfa4c7ebe25fe96b7f2c2aa87
xiaoyonga <845219159@qq.com>
2016-10-18 15:44:05 +0800

    dsadsad

--- 启动停止脚本/git自动拉取代码脚本.py
+++ 启动停止脚本/git自动拉取代码脚本.py
@@ -3,11 +3,12 @@ import os
 import sys
 import shutil
 projcet="ALL/"
+user="xiaoyong/"
 def cmd(version):
-    res=os.system("mkdir -p /code/xiaoyong/")
+    res=os.system("mkdir -p /code/"+user)
     if res==0:
         print("代码目录创建成功")
-        os.chdir("/code/xiaoyong/")
+        os.chdir("/code/"+user)
         res=os.system("git clone https://github.com/xiaoyongaa/ALL.git")
         if res==0:
             msg="代码拉取成功！！！"
@@ -19,10 +20,10 @@ def cmd(version):
                 res=os.system("tar -zcvf ALL_"+version+".tar.gz --exclude=.*   *")
                 if res==0:
                     print("代码压缩成功")
-                    res=os.system("mv *.tar.gz /code/xiaoyong/")
+                    res=os.system("mv *.tar.gz /code/"+user)
                     if res==0:
                         print("代码移动成功！")
-                        shutil.rmtree("/code/xiaoyong/ALL")  #原来代码删除
+                        shutil.rmtree("/code/"+user+projcet)  #原来代码删除
                         #代码推送模块
                     else:
                         print("代码移动失败！")
--- 网络编程第四周/生产者消费者模型.py
+++ 网络编程第四周/生产者消费者模型.py
@@ -1,9 +1,14 @@
+import queue
+q=queue.Queue(20)
+
+
+def productor():
+    q.put("包子")
 
 
 
-
-
-
+def connsumer():
+    q.get()
 
 
 
