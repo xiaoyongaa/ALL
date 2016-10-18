95bfd1244298ee7c2540ea8775be1453d5487b42
xiaoyonga <845219159@qq.com>
2016-10-18 15:32:47 +0800

    dsadsad

---
+++ 启动停止脚本/git自动拉取代码脚本.py
@@ -0,0 +1,48 @@
+#!/app/python3.5/bin/python3.5
+import os
+import sys
+import shutil
+projcet="ALL/"
+def cmd(version):
+    res=os.system("mkdir -p /code/xiaoyong/")
+    if res==0:
+        print("代码目录创建成功")
+        os.chdir("/code/xiaoyong/")
+        res=os.system("git clone https://github.com/xiaoyongaa/ALL.git")
+        if res==0:
+            msg="代码拉取成功！！！"
+            print(msg)
+            os.chdir(projcet)
+            res=os.system("git checkout"+" "+version)
+            if res==0:
+                msg="{name}项目{v}版本代码切换成功！！！".format(name=projcet,v=version)
+                res=os.system("tar -zcvf ALL_"+version+".tar.gz --exclude=.*   *")
+                if res==0:
+                    print("代码压缩成功")
+                    res=os.system("mv *.tar.gz /code/xiaoyong/")
+                    if res==0:
+                        print("代码移动成功！")
+                        shutil.rmtree("/code/xiaoyong/ALL")  #原来代码删除
+                        #代码推送模块
+                    else:
+                        print("代码移动失败！")
+                else:
+                    print("代码压缩失败")
+            else:
+                print("版本切换失败")
+        else:
+            print("代码拉取失败")
+    else:
+        print("代码目录创建失败")
+
+
+
+def main():
+    try:
+       version=sys.argv[1]
+       print(version)
+       cmd(version)
+    except Exception as ex:
+        print("脚本的第一个版本传承不能为空，请输入版本号")
+
+main()
