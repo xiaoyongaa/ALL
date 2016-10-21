38af3966b70d013bb0b698149db354b7a5f803a0
xiaoyonga <845219159@qq.com>
2016-10-21 22:29:38 +0800

    aa

--- 网络编程第四周/自定义线程池 2.py
+++ 网络编程第四周/自定义线程池 2.py
@@ -1,6 +1,16 @@
 #线程只能创建这么多，再创建就不行了。
-#import threadpool
-
+# import threadpool
+# import time
+# def task(a):
+#     print(a)
+#
+#
+# pool=threadpool.ThreadPool(100)
+# res=threadpool.makeRequests(task,args_list=["11111",])
+# for i in  res:
+#     pool.putRequest(i)
+#
+# pool.wait()
 
 
 
